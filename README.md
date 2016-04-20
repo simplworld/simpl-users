87cb19a2131a9a80ff203f64106dbc24478b86d9
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-19 22:14:06 -0500

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+Wharton simpl_users app
+=======================
+
+A reusable application to define SIMPL Users
+
+Running the tests
+-----------------
+
+To run the tests issue the command:
+
+  py.test --nomigrations
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='simpl-users',
+    version="0.1.0",
+    description='simpl-users provides Wharton SIMPL Users',
+    long_description='',
+    author='Jeff Triplett',
+    author_email='jeff@revsys.com',
+    url='https://stash.wharton.upenn.edu/projects/WCIT/repos/simpl-users/browse',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+)
