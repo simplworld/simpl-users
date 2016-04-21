2a7d922867c3099815e222598fab467d3d34e61a
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-21 10:49:00 -0500

    Adds User and Faculty factories

---
+++ simpl_users/factories.py
@@ -0,0 +1,18 @@
+import factory
+
+from .models import User
+
+
+class UserFactory(factory.DjangoModelFactory):
+    username = factory.Sequence(lambda n: 'user%s' % n)
+    first_name = factory.Sequence(lambda n: 'User%s Bob' % n)
+    last_name = factory.Sequence(lambda n: 'User%s Smith' % n)
+    email = factory.Sequence(lambda n: 'user%s@example.com' % n)
+
+    class Meta:
+        django_get_or_create = ('username', )
+        model = User
+
+
+class FacultyFactory(UserFactory):
+    faculty = True
