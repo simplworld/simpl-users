c58471a4d615a8831b28a1f305c565b589ee086f
Jeff Triplett <jeff.triplett@gmail.com>
2016-05-12 12:57:37 -0500

    Adds username filter and updates api docs

--- simpl_users/apis/views.py
+++ simpl_users/apis/views.py
@@ -25,6 +25,7 @@ class UserViewSet(CommonViewSet):
         'is_active',
         'is_staff',
         'is_superuser',
+        'username',
         'subscriber_code',
     )
     ordering_fields = ()
@@ -50,27 +51,32 @@ class UserViewSet(CommonViewSet):
               type: integer
               paramType: query
               required: false
-              description: Filters User per canvas_id via canvas_id
+              description: Filters User per Canvas ID via canvas_id
             - name: is_active
               type: boolean
               paramType: query
               required: false
-              description: Filters User per is_active via is_active
+              description: Filters User per Is Active via is_active
             - name: is_staff
               type: boolean
               paramType: query
               required: false
-              description: Filters User per is_staff via is_staff
+              description: Filters User per Is Staff via is_staff
             - name: is_superuser
               type: boolean
               paramType: query
               required: false
-              description: Filters User per is_superuser via is_superuser
+              description: Filters User per Is Superuser via is_superuser
             - name: subscriber_code
               type: string
               paramType: query
               required: false
-              description: Filters User per subscriber_code via subscriber_code
+              description: Filters User per Subscriber Code via subscriber_code
+            - name: username
+              type: string
+              paramType: query
+              required: false
+              description: Filters User per Username via username
         """
         return super(UserViewSet, self).list(request)
 
