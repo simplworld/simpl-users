755038d8ac52857c5c1bed1bc5583dd2e68662db
Jeff Triplett <jeff.triplett@gmail.com>
2016-05-12 10:17:40 -0500

    Addresses comments from ticket (PDLL-11)

--- simpl_users/managers.py
+++ simpl_users/managers.py
@@ -5,21 +5,9 @@ from django.contrib.auth.models import UserManager
 class UserQuerySet(models.QuerySet):
     """ QuerySet to differentiate between faculty and students """
 
-    def faculty(self):
-        return self.filter(faculty=True)
-
-    def students(self):
-        return self.filter(student=True)
-
 
 class UserManager(UserManager):
     """ Corresponding manager to differentiate between faculty and students """
 
     def get_queryset(self):
         return UserQuerySet(self.model, using=self._db)
-
-    def faculty(self):
-        return self.get_queryset().faculty()
-
-    def students(self):
-        return self.get_queryset().students()
