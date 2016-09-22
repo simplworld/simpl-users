7e56fe5e21b18f2a962b49a1d09c4954b06d1a42
Frank Wiles <frank@revsys.com>
2016-09-22 10:09:04 -0500

    Debug cleanup

--- simpl_users/apis/views.py
+++ simpl_users/apis/views.py
@@ -31,18 +31,6 @@ class UserViewSet(CommonViewSet):
     lookup_field = 'username'
     ordering_fields = ()
 
-    #def create(self, request):
-    #    """
-    #    Add a new User
-    #    """
-    #    validated_user = super(UserViewSet, self).create(request)
-    #    from IPython import embed; embed()
-    #    if request.data.get('password', False):
-    #        validated_user.set_password(request.data['password'])
-    #        validated_user.save()
-#
-#        return validated_user
-
     def destroy(self, request, username=None):
         """
         Delete an User
