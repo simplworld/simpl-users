20e7be3d3b586e409d3b51aa7b8d81e23229291a
Flavio Curella <flavio.curella@gmail.com>
2017-03-09 15:08:19 -0600

    Bump version: 0.3.1 → 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpl-users',
-    version="0.3.1",
+    version="0.3.2",
     description='simpl-users provides Wharton SIMPL Users',
     long_description='',
     author='Jeff Triplett',
