8652f1e70ed011ff9fed1b0d38c77261a77e89e4
Jane Eisenstein <ejane@wharton.upenn.edu>
2018-03-16 10:25:16 -0400

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpl-users',
-    version="0.3.6",
+    version="0.3.7",
     description='simpl-users provides Wharton SIMPL Users',
     long_description='',
     author='Jeff Triplett',
