f5557766959983a07862b77b1ec17a56a9731612
Jane Eisenstein <ejane@wharton.upenn.edu>
2018-09-14 10:13:16 -0400

    Bump version: 0.7.2 → 0.7.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='simpl-users',
-    version="0.7.2",
+    version="0.7.3",
     description='simpl-users provides Simpl Users',
     long_description='readme',
     author='Jeff Triplett',
