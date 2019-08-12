ddf638bd1800402c7cb38d00b200d09fd0b7f9ff
Jane Eisenstein <ejane@wharton.upenn.edu>
2019-08-12 13:21:49 -0400

    Bump version: 0.7.6 → 0.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ f.close()
 
 setup(
     name='simpl-users',
-    version="0.7.6",
+    version="0.8.0",
     description='simpl-users provides Simpl Users',
     long_description=readme,
     long_description_content_type="text/markdown",
