b4c28ff709ecefab82c1bb995598df516dc50231
Jane Eisenstein <ejane@softweave.com>
2020-09-07 14:40:55 -0400

    Bump version: 0.8.3 → 0.8.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
 
 setup(
     name='simpl-users',
-    version="0.8.3",
+    version="0.8.4",
     description='simpl-users provides Simpl Users',
     long_description=readme,
     long_description_content_type="text/markdown",
